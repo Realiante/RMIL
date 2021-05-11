348a342c70a3ce77c8c74444278b7cf97b20ac32
Realiante <RealiantG@Gmail.com>
2021-05-11 14:28:23 +0300

    WIP: structure for remote server objects

--- RMI-L-Client/src/main/java/dev/rea/rmil/client/manager/RemoteServer.java
+++ RMI-L-Client/src/main/java/dev/rea/rmil/client/manager/RemoteServer.java
@@ -1,24 +1,55 @@
 package dev.rea.rmil.client.manager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rea.dev.rmil.remote.RemoteExecutorContainer;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.UUID;
 
 public class RemoteServer {
 
-    private final String address;
-    private final AtomicInteger maxTasks;
+    private static final Logger logger = LoggerFactory.getLogger(RemoteServer.class);
 
-    public RemoteServer(String address, int maxTasks) {
+    public final UUID serverID;
+    private final String address;
+    private final int port;
+
+    private boolean loaded;
+    private final int maxTasks;
+    private Registry registry;
+    private RemoteExecutorContainer executorContainer;
+
+    public RemoteServer(String address, int port) {
         this.address = address;
-        this.maxTasks = new AtomicInteger(maxTasks);
+        this.port = port;
+        this.maxTasks = 0;
+        this.serverID = UUID.fromString(String.format("%s:%s", address, port));
+
+        try {
+            this.registry = LocateRegistry.getRegistry(address, port);
+            this.executorContainer = (RemoteExecutorContainer) registry.lookup("engine");
+        } catch (RemoteException | NotBoundException exception) {
+            var serverName = String.format("%s:%s", address, port);
+            logger.debug("Failed to locate server " + serverName, exception);
+            logger.info(String.format("Server %s is not bound or RemoteException occurred", serverName));
+        }
     }
 
     public String getAddress() {
         return address;
     }
 
-    public AtomicInteger getMaxTasks() {
+    public int getMaxTasks() {
         return maxTasks;
+    }
+
+    public boolean isLoaded() {
+        return loaded;
     }
 
     @Override
@@ -26,11 +57,11 @@ public class RemoteServer {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         RemoteServer that = (RemoteServer) o;
-        return address.equals(that.address);
+        return port == that.port && address.equals(that.address);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(address);
+        return Objects.hash(address, port);
     }
 }
---
+++ RMI-L-Client/src/main/java/dev/rea/rmil/client/manager/ServerAddress.java
@@ -0,0 +1,19 @@
+package dev.rea.rmil.client.manager;
+
+public final class ServerAddress {
+    private final String address;
+    private final int port;
+
+    public ServerAddress(String address, int port) {
+        this.address = address;
+        this.port = port;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
