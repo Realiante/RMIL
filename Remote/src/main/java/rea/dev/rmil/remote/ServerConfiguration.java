a413f6106893b06be65b90dd07b3518808bc4c20
Realiante <RealiantG@Gmail.com>
2021-06-15 23:29:27 +0300

    Fixed oversight where ServerConfiguration.java was not serializable

--- Remote/src/main/java/rea/dev/rmil/remote/ServerConfiguration.java
+++ Remote/src/main/java/rea/dev/rmil/remote/ServerConfiguration.java
@@ -1,17 +1,20 @@
 package rea.dev.rmil.remote;
 
+import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
-public final class ServerConfiguration {
+public final class ServerConfiguration implements Serializable {
+    static final long serialVersionUID = 2323415151251L;
 
     private final UUID serverID;
     private final int maxThreads;
     private final Priority priority;
 
     public ServerConfiguration(UUID serverID, int maxThreads, Priority priority) {
-        this.serverID = serverID;
+        this.serverID = Objects.requireNonNull(serverID);
         this.maxThreads = maxThreads;
-        this.priority = priority;
+        this.priority = Objects.requireNonNull(priority);
     }
 
     public UUID getServerID() {
@@ -26,7 +29,21 @@ public final class ServerConfiguration {
         return priority;
     }
 
-    public enum Priority {
-        NORMAL, LOW
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ServerConfiguration)) return false;
+        ServerConfiguration that = (ServerConfiguration) o;
+        return maxThreads == that.maxThreads && serverID.equals(that.serverID) && priority == that.priority;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(serverID, maxThreads, priority);
+    }
+
+    public enum Priority implements Serializable {
+        NORMAL, LOW;
+        static final long serialVersionUID = 2323415151251886L;
     }
 }
